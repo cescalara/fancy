01a18691b9730752647906f47e8b42534978d686
Francesca Capel <capel.francesca@gmail.com>
2018-04-23 18:29:37 +0200

    Tidied up and tested the Data() class and Data.show()

--- fancy/__init__.py
+++ fancy/__init__.py
@@ -1,3 +1,1 @@
 from .data import * 
-from .source import *
-from .uhecr import *
--- fancy/data/data.py
+++ fancy/data/data.py
@@ -5,20 +5,20 @@ from matplotlib import pyplot as plt
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
+from ..allskymap import AllSkyMap
+from ..utils import PlotStyle
+
 __all__ = ['Data', 'Source', 'Uhecr']
+
 
 class Data():
     """
-    A container for storage of data.
-
-    Parses information for known data files in txt format. 
+    A container for high level storage of data.
     """
 
     def __init__(self):
         """
-        A container for storage of data.
-        
-        Parses information for known data files in txt format. 
+        A container for high level storage of data.      
         """
 
         self._filename = None
@@ -30,94 +30,6 @@ class Data():
         self.source = {}        
 
         
-    def _define_type(self):
-        """
-        Determine the file type for parsing.
-
-        :return: array of names of cols in file
-        """
-        if 'UHECR' in self._filename:
-            self._filetype = 'uhecr'
-            self._filelayout = ['year', 'day', 'incidence angle',
-                      'energy', 'ra', 'dec',
-                      'glon', 'glat']
-
-        elif 'agn' in self._filename:
-            self._filetype = 'agn'
-            self._filelayout = ['name', 'glon', 'glat', 'D']
-        
-        else:
-            print ('File layout not recognised')
-            self._filetype = None 
-            self._filelayout = None
-
-
-    
-    def _parse(self):
-        """
-        Parse the data form the object's file.
-        
-        :return: arrays for each column in the data file
-        """
-
-        output = pd.read_csv(self._filename, comment = '#',
-                             delim_whitespace = True,
-                             names = self._filelayout)
-
-        output_dict = output.to_dict()
-        
-        return output_dict
-
-
-    def get_len(self):
-        """
-        Get the length of the data set.
-
-        :return: the length of the data set
-        """
-
-        if self._filetype == 'uhecr':
-            n = len(self.data['year'])
-
-        elif self._filetype == 'agn':
-            n = len(self.data['name'])
-
-        return n
-
-    
-    def get_coordinates(self):
-        """
-        Get the galactic coordinates from self.data
-        and return them as astropy SkyCoord
-        
-        :return: astropy.coordinates.SkyCoord
-        """
-
-        glon = np.array( list(self.data['glon'].values()) )
-        glat = np.array( list(self.data['glat'].values()) )
-
-        return SkyCoord(l = glon * u.degree, b = glat * u.degree, frame = 'galactic')
-
-    
-    
-    def get_by_name(self, name):
-        """
-        Get data entries by name.
-
-        :param name: name of the data as in self._filelayout
-        :return: an array of data entries
-        """
-
-        try:
-            selected_data = np.array( list(self.data[name].values()) )
-
-        except ValueError:
-            print ('No data of type', name)
-            selected_data = []
-
-        return selected_data
-
-
     def add_source(self, filename, label = None):
         """
         Add a source object to the data cotainer
@@ -125,11 +37,8 @@ class Data():
         :param filename: name of the file containing the object's data
         :param label: reference label for the source object
         """
-        self._filename = filename
-        self._define_type()
-        self._data = self._parse()
-
-        new_source = Source(self._data)
+        raw_data = RawData(filename)
+        new_source = Source(raw_data)
 
         # generate numbered labels by default
         if label == None:
@@ -146,11 +55,8 @@ class Data():
         :param filename: name of the file containing the object's data
         :param label: reference label for the source object
         """
-        self._filename = filename
-        self._define_type()
-        self._data = self._parse()
-
-        new_uhecr = Uhecr(self._data)
+        raw_data = RawData(filename)
+        new_uhecr = Uhecr(raw_data)
 
         # generate numbered labels by default
         if label == None:
@@ -169,13 +75,13 @@ class Data():
         return 0
 
 
-    def _add_skymap_labels(self):
+    def _add_skymap_labels(self, style, skymap):
         """
         Add the standard labels for parallels and meridians to the skymap
         """
 
         # map background, parallels, meridians and labels
-        skymap.drawmapboundary(fill_color = cmap(0))
+        skymap.drawmapboundary(fill_color = style.cmap(0))
         skymap.drawparallels(np.arange(-75, 76, 15), linewidth = 1, dashes = [1,2],
                              labels=[1, 0, 0, 0], textcolor = 'white', fontsize = 14, alpha = 0.7);
         # workaround for saving properly
@@ -187,7 +93,7 @@ class Data():
                              alpha = 0.7)  
 
 
-    def _add_uhecr_colorbar(self):
+    def _add_uhecr_colorbar(self, style):
         """
         Add a colorbar normalised over all the Uhecr energies.
         
@@ -221,42 +127,141 @@ class Data():
         style = PlotStyle()
 
         # figure
-        if fig == None:
-            fig = plt.figure(figsize = (12, 6));
+        fig = plt.figure(figsize = (12, 6));
         ax = plt.gca()
 
         # skymap
-        if skymap == None:
-            skymap = AllSkyMap(projection = 'hammer', lon_0 = 0, lat_0 = 0);
+        skymap = AllSkyMap(projection = 'hammer', lon_0 = 0, lat_0 = 0);
 
         # iterate over the uhecr objects
         if self.uhecr != {}:
-            for label, uhecr in d.items():
-                uhecr.plot(style, label)
+            for label, uhecr in self.uhecr.items():
+                uhecr.plot(style, label, skymap)
 
         # add a colorbar
-        #self._add_uhecr_colorbar()
+        #self._add_uhecr_colorbar(style)
         
         # iterate over the source objects
         if self.source != {}:
-            for label, source in d.items():
-                source.plot(style, label)
+            for label, source in self.source.items():
+                source.plot(style, label, skymap)
 
         # iterate over the detector objects
         # add this
         
         # standard labels and background
-        self._add_skymap_labels()
+        self._add_skymap_labels(style, skymap)
         
         # legend
-        ax = fig.gca()
         plt.legend(bbox_to_anchor=(0.85, 0.85))
         leg = ax.get_legend()
         leg.legendHandles[0].set_color(style.cmap(0.4))
         for text in leg.get_texts():
             plt.setp(text, color = style.textcolor, alpha = 0.7)
         
+
+class RawData():
+    """
+    Parses information for known data files in txt format. 
+    """
+
+    def __init__(self, filename):
+        """
+        Parses information for known data files in txt format. 
+        """
+
+        self._filename = filename
+        self._define_type()
+        self._data = self._parse()
         
+
+    def _define_type(self):
+        """
+        Determine the file type for parsing.
+        
+        :return: array of names of cols in file
+        """
+        if 'UHECR' in self._filename:
+            self._filetype = 'uhecr'
+            self._filelayout = ['year', 'day', 'incidence angle',
+                                'energy', 'ra', 'dec',
+                                'glon', 'glat']
+
+        elif 'agn' in self._filename:
+            self._filetype = 'agn'
+            self._filelayout = ['name', 'glon', 'glat', 'D']
+            
+        else:
+            print ('File layout not recognised')
+            self._filetype = None 
+            self._filelayout = None
+            
+
+    
+    def _parse(self):
+        """
+        Parse the data form the object's file.
+        
+        :return: arrays for each column in the data file
+        """
+
+        output = pd.read_csv(self._filename, comment = '#',
+                             delim_whitespace = True,
+                             names = self._filelayout)
+
+        output_dict = output.to_dict()
+        
+        return output_dict
+
+
+    def get_len(self):
+        """
+        Get the length of the data set.
+
+        :return: the length of the data set
+        """
+
+        if self._filetype == 'uhecr':
+            n = len(self._data['year'])
+
+        elif self._filetype == 'agn':
+            n = len(self._data['name'])
+
+        return n
+
+    
+    def get_coordinates(self):
+        """
+        Get the galactic coordinates from self.data
+        and return them as astropy SkyCoord
+        
+        :return: astropy.coordinates.SkyCoord
+        """
+
+        glon = np.array( list(self._data['glon'].values()) )
+        glat = np.array( list(self._data['glat'].values()) )
+
+        return SkyCoord(l = glon * u.degree, b = glat * u.degree, frame = 'galactic')
+
+    
+    
+    def get_by_name(self, name):
+        """
+        Get data entries by name.
+
+        :param name: name of the data as in self._filelayout
+        :return: an array of data entries
+        """
+
+        try:
+            selected_data = np.array( list(self._data[name].values()) )
+
+        except ValueError:
+            print ('No data of type', name)
+            selected_data = []
+
+        return selected_data
+            
         
 class Source():
     """
@@ -264,25 +269,25 @@ class Source():
     """
 
     
-    def __init__(self, data):
+    def __init__(self, raw_data):
         """
         Stores the data and parameters for sources.
         
         :param data: data passed as an instance of Data
         """
 
-        self.N = data.get_len()
+        self.N = raw_data.get_len()
         
-        self.coord = data.get_coordinates()
+        self.coord = raw_data.get_coordinates()
 
-        self.distance = data.get_by_name('D') # in Mpc
+        self.distance = raw_data.get_by_name('D') # in Mpc
 
-        self.name = data.get_by_name('name')
+        self.name = raw_data.get_by_name('name')
 
         self.label = 'Source'
 
         
-    def plot(self, style, label):
+    def plot(self, style, label, skymap):
         """
         Plot the sources on a map of the sky. 
 
@@ -312,31 +317,31 @@ class Uhecr():
     """
 
     
-    def __init__(self, data):
+    def __init__(self, raw_data):
         """
         Stores the data and parameters for UHECRs.
         
         :param data: data passed as an instance of Data
         """
 
-        self.N = data.get_len()
+        self.N = raw_data.get_len()
         
-        self.coord = data.get_coordinates()
+        self.coord = raw_data.get_coordinates()
 
-        self.year = data.get_by_name('year')
+        self.year = raw_data.get_by_name('year')
 
-        self.day = data.get_by_name('day')
+        self.day = raw_data.get_by_name('day')
 
-        self.incidence_angle = data.get_by_name('incidence angle')
+        self.incidence_angle = raw_data.get_by_name('incidence angle')
 
-        self.energy = data.get_by_name('energy')
+        self.energy = raw_data.get_by_name('energy')
 
         self.coord_uncertainty = 4.0 # uncertainty in degrees
 
         self.label = 'UHECR'
 
 
-    def plot(self, style, label):
+    def plot(self, style, label, skymap):
         """
         Plot the Uhecr instance on a skymap.
 
