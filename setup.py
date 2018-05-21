5d17ed82798ea543932152f31c4f42c5d57be2ea
Francesca Capel <capel.francesca@gmail.com>
2018-05-21 14:27:14 +0200

    Add package setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+
+    name = 'fancy',
+    packages = ['fancy'], 
+    version = 'v0.1',
+    description = 'Framework for the Analysis of Cosmic raYs',
+    author = 'Francesca Capel',
+    author_email = 'capel.francesca@gmail.com',
+    url = 'https://github.com/cescalara/fancy',
+    download_url = 'https://github.com/cescalara/fancy/archive/v0.1.tar.gz',
+    keywords = ['UHECR', 'analysis'], 
+    classifiers = [],
+
+)
