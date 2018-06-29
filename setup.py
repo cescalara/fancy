80ef2b56349f7fffb79b69398b2aa1fa8cf62e4d
Francesca Capel <capel.francesca@gmail.com>
2018-06-29 15:59:18 +0200

    Update the version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
 
     name = 'fancy',
     packages = setuptools.find_packages(), 
-    version = 'v1.0',
+    version = 'v1.1',
     description = 'Framework for the Analysis of Cosmic raYs',
     author = 'Francesca Capel',
     author_email = 'capel.francesca@gmail.com',
