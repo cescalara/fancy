342606c96431926c654bcdd5c8304a80b1bc6151
Francesca Capel <capel.francesca@gmail.com>
2019-01-31 17:20:39 +0100

    Fix corner for case with no truths

--- fancy/plotting/corner.py
+++ fancy/plotting/corner.py
@@ -13,7 +13,7 @@ class Corner():
     Simple corner plotting.
     """
 
-    def __init__(self, data_frame, truth, color = purple, levels = [0.99, 0.9, 0.6, 0.3], fontsize = 22):
+    def __init__(self, data_frame, truth = None, color = purple, levels = [0.99, 0.9, 0.6, 0.3], fontsize = 22):
         """
         Simple corner plotting.
         
@@ -38,20 +38,31 @@ class Corner():
         pairgrid = pairgrid.map_diag(sns.kdeplot, color = color, shade = True, lw = 2, zorder = 2)
         pairgrid = pairgrid.map_offdiag(plot_HPD_levels, color = color, shade = True, levels = levels)
 
-        # truths
-        N = len(truth)
-        for i, t in enumerate(truth):
-            for j in range(N):
-                pairgrid.axes[j, i].axvline(t, lw = 2, color = 'k', zorder = 5, alpha = 0.7)
-            for k in range(N):
-                if i != k:
-                    pairgrid.axes[i, k].axhline(t, lw = 2, color = 'k', zorder = 5, alpha = 0.7)
-                    pairgrid.axes[i, k].spines['right'].set_visible(True)
-                    pairgrid.axes[i, k].spines['top'].set_visible(True) 
-                else:
-                    sns.despine(ax = pairgrid.axes[i, k])
-                    
+        # Truths
+        if truth:
+        
+            N = len(truth)
+            for i, t in enumerate(truth):
+                for j in range(N):
+                    pairgrid.axes[j, i].axvline(t, lw = 2, color = 'k', zorder = 5, alpha = 0.7)
+                    for k in range(N):
+                        if i != k:
+                            pairgrid.axes[i, k].axhline(t, lw = 2, color = 'k', zorder = 5, alpha = 0.7)
+                            pairgrid.axes[i, k].spines['right'].set_visible(True)
+                            pairgrid.axes[i, k].spines['top'].set_visible(True) 
+                        else:
+                            sns.despine(ax = pairgrid.axes[i, k])
+        else:
 
+            N = np.shape(data_frame)[1]
+            for j in range(N):
+                for k in range(N):
+                    if i != k:
+                        pairgrid.axes[i, k].spines['right'].set_visible(True)
+                        pairgrid.axes[i, k].spines['top'].set_visible(True) 
+                    else:                   
+                        sns.despine(ax = pairgrid.axes[i, k])
+     
         # Tidy axes
         sns.despine(ax = pairgrid.axes[0, 0], left = True)
         pairgrid.axes[0, 0].get_yaxis().set_visible(False)
