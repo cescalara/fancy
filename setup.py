8195290211a4ac9e9c09b9cc5cabff3d93313f5c
Francesca Capel <capel.francesca@gmail.com>
2019-02-01 13:58:34 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setuptools.setup(
 
     name = 'fancy',
     packages = setuptools.find_packages(), 
-    version = 'v2.0.4',
+    version = 'v2.1.0',
     description = 'Framework for the Analysis of Cosmic raYs',
     author = 'Francesca Capel',
     author_email = 'capel.francesca@gmail.com',
     url = 'https://github.com/cescalara/fancy',
-    download_url = 'https://github.com/cescalara/fancy/archive/v0.9.tar.gz',
     keywords = ['UHECR', 'analysis'], 
     classifiers = [],
 
