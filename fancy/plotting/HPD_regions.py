53d9dccf04c4ff4dc54533411cc550bb408fe216
Keito Watanabe <k.wat8973@gmail.com>
2021-06-29 11:26:08 +0900

    FIxed float to int bug

--- fancy/plotting/HPD_regions.py
+++ fancy/plotting/HPD_regions.py
@@ -10,9 +10,9 @@ def grid_HPD_contours(data, levels):
     specified levels as a fraction the integral over the whole grid.
     """
 
-    N = 1e3
+    N = int(1e3)
     thresholds = np.linspace(0, data.max(), N)
-    integral = ((data >= thresholds[:, None, None]) * data).sum(axis=(1,2))
+    integral = ((data >= thresholds[:, None, None]) * data).sum(axis=(1, 2))
     norm = integral[0]
 
     # Interpolate required threshold from integral
@@ -22,14 +22,14 @@ def grid_HPD_contours(data, levels):
     return contours
 
 
-def HPD_contours(xdata, ydata, levels, bins = 50):
+def HPD_contours(xdata, ydata, levels, bins=50):
     """
     Estimate the HPD contours from sample chains (xdata, ydata).
     Output are levels which can be input into seaborn KDE plots.
     """
 
     # 2D normed histogram
-    H, xedges, yedges = np.histogram2d(xdata, ydata, bins = bins, normed = True)
+    H, xedges, yedges = np.histogram2d(xdata, ydata, bins=bins, normed=True)
     norm = H.sum()
 
     # Set target levels as percentage of norm
@@ -43,26 +43,26 @@ def HPD_contours(xdata, ydata, levels, bins = 50):
     # Find levels by summing up histogram to target
     output_levels = []
     for t in target:
-        output_levels.append(bisect(objective, H.min(), H.max(), args = (t,)))
+        output_levels.append(bisect(objective, H.min(), H.max(), args=(t,)))
 
     # Define bound level for shading in seaborn KDE.
     output_levels.append(H.max())
     return output_levels
 
 
-def plot_HPD_levels(xdata, ydata, levels = [0.99, 0.9, 0.6, 0.3], 
-                    bins = 50, **kwargs):
+def plot_HPD_levels(xdata, ydata, levels=[0.99, 0.9, 0.6, 0.3],
+                    bins=50, **kwargs):
     """
     Plot a KDE plot for the data with specified levels.
     """
 
     new_levels = HPD_contours(xdata, ydata, levels, bins)
-    sns.kdeplot(xdata, ydata, n_levels = new_levels,
+    sns.kdeplot(xdata, ydata, n_levels=new_levels,
                 **kwargs)
 
-    
-def plot_HPD_levels_multi(xdata, ydata, levels = [0.99, 0.9, 0.6, 0.3], 
-                    bins = 50, **kwargs):
+
+def plot_HPD_levels_multi(xdata, ydata, levels=[0.99, 0.9, 0.6, 0.3],
+                          bins=50, **kwargs):
     """
     Plot a KDE plot for the data with specified levels.
     Handles multiple colors for same dataframe.
@@ -70,5 +70,5 @@ def plot_HPD_levels_multi(xdata, ydata, levels = [0.99, 0.9, 0.6, 0.3],
 
     my_color = next(plot_HPD_levels.color_cycle)
     new_levels = HPD_contours(xdata, ydata, levels, bins)
-    sns.kdeplot(xdata, ydata, n_levels = new_levels, normed = True,
-                color = my_color, shade = True, alpha = 0.5)
+    sns.kdeplot(xdata, ydata, n_levels=new_levels, normed=True,
+                color=my_color, shade=True, alpha=0.5)
