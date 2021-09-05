adc8b910d881d0e3e69c73d46691d2e0ce324ba7
Keito <k.wat8973@gmail.com>
2021-09-05 23:21:28 +0900

    Added new features to plotting

--- fancy/plotting/allskymap_cartopy.py
+++ fancy/plotting/allskymap_cartopy.py
@@ -8,8 +8,8 @@ import astropy.units as u
 
 from ..detector.exposure import m_dec
 
-
 import cartopy.crs as ccrs
+
 
 # To allow the geodesics to be smoother, the threshold must be more lower.
 # So we construct a simple class and change the internal property of ccrs.Mollweide
@@ -19,7 +19,9 @@ class LowerThresholdMollweide(ccrs.Mollweide):
     def threshold(self):
         return 1e4
 
+
 __all__ = ['AllSkyMapCartopy']
+
 
 class AllSkyMapCartopy():
     '''
@@ -36,8 +38,12 @@ class AllSkyMapCartopy():
     - Since cartopy only supports Mollweide projections, this is used instead.
     - Since we work with lon / lat coordinates, ccrs.PlateCarree coordinates will be used for transformations
     '''
-    
-    def __init__(self, projection="moll", lon_0 = 0., figsize=(12,7), fig_params={}, ax_params = {}):
+    def __init__(self,
+                 projection="moll",
+                 lon_0=0.,
+                 figsize=(12, 7),
+                 fig_params={},
+                 ax_params={}):
         '''
         Initializes using plt.axes. Returns matplotlib.axes.Axes object.
 
@@ -53,7 +59,8 @@ class AllSkyMapCartopy():
         else:
             raise ValueError("Only moll projections are allowed.")
 
-        if plt.gca():
+        if plt.gcf():
+            plt.clf()
             self.fig = plt.figure(figsize=figsize, **fig_params)
             self.ax = plt.gca(projection=proj, **ax_params)
 
@@ -61,15 +68,26 @@ class AllSkyMapCartopy():
             self.fig = plt.figure(figsize=figsize, **fig_params)
             self.ax = plt.axes(projection=proj, **ax_params)
 
-        self.lon_0 = lon_0   # for shifting with labels later on
+        self.lon_0 = lon_0  # for shifting with labels later on
 
         # initial configurations
         self.set_extent()
 
-
-    def add_grid_labels(self, dx, dy, xlims, ylims,
-                        lft=False, rgt=False, top=False, bot=False, mid=False, spherical=False):
-        """Add grid line labels manually for projections that aren't supported. 
+    def add_grid_labels(self,
+                        dx,
+                        dy,
+                        xlims,
+                        ylims,
+                        lft=False,
+                        rgt=False,
+                        top=False,
+                        bot=False,
+                        mid=False,
+                        ynudge=5,
+                        spherical=False,
+                        reverse=False):
+        """
+        Add grid line labels manually for projections that aren't supported. 
         Obtained from https://github.com/SciTools/cartopy/issues/881
 
         Args:
@@ -80,9 +98,11 @@ class AllSkyMapCartopy():
             top (bool): whether to label the top side
             bot (bool): whether to label the bottom side
             spherical (bool): pad the labels better if a side of ax is spherical
+            reverse (bool) : run the longitudes in reverse
         """
+
         x0, x1, dx = xlims[0], xlims[1], dx
-        y0, y1, dy = ylims[0], ylims[1] + dy, dy
+        y0, y1, dy = ylims[0], ylims[1], dy
 
         if dx <= 10:
             dtype = float
@@ -95,20 +115,38 @@ class AllSkyMapCartopy():
             dtype = int
 
         for lon in np.arange(x0 + dx, x1, dx, dtype=dtype):
+            if reverse:
+                if lon <= 0:
+                    label = r'{0}$^\circ$'.format(np.abs(lon))
+                if lon > 0:
+                    label = r'-{0}$^\circ$'.format(np.abs(lon))
+            else:
+                label = '{0}$\degree$'.format(lon)
+
             if top:
-                text = self.ax.text(lon, y1, r'{0}$^\circ$\n\n'.format(lon),
-                            va='center', ha='center',
-                            transform=ccrs.PlateCarree())
+                text = self.ax.text(lon,
+                                    y1,
+                                    r'{0}$^\circ$\n\n'.format(lon),
+                                    va='center',
+                                    ha='center',
+                                    transform=ccrs.PlateCarree())
             if bot:
-                text = self.ax.text(lon, y0, r'\n\n{0}$^\circ$'.format(lon),
-                            va='center', ha='center',
-                            transform=ccrs.PlateCarree())
+                text = self.ax.text(lon,
+                                    y0,
+                                    r'\n\n{0}$^\circ$'.format(lon),
+                                    va='center',
+                                    ha='center',
+                                    transform=ccrs.PlateCarree())
 
             if mid:
-                ymid = (y0 + y1) / 2. - 20
-                text = self.ax.text(lon, ymid, r'${0}^\circ$'.format(lon),
-                    va='center', ha='center',
-                    transform=ccrs.PlateCarree())
+                ymid = (y0 + y1) / 2. - ynudge
+                text = self.ax.text(lon,
+                                    ymid,
+                                    label,
+                                    va='center',
+                                    ha='center',
+                                    transform=ccrs.PlateCarree(),
+                                    fontsize=10)
 
         for lat in np.arange(y0 + dy, y1, dy, dtype=dtype):
             if spherical:
@@ -121,28 +159,81 @@ class AllSkyMapCartopy():
             else:
                 va = 'center'
             if lft:
-                text = self.ax.text(x0, lat, r'${0}^\circ$       '.format(lat), va=va, ha='left',
-                            transform=ccrs.PlateCarree())
+                text = self.ax.text(x0,
+                                    lat,
+                                    '${0}\degree$     '.format(lat),
+                                    va=va,
+                                    ha='right',
+                                    transform=ccrs.PlateCarree(),
+                                    fontsize=12)
             if rgt:
-                text = self.ax.text(x1, lat, r'       {0}$^\circ$'.format(lat), va=va, ha='left',
-                            transform=ccrs.PlateCarree())
+                text = self.ax.text(x1,
+                                    lat,
+                                    r'       {0}$^\circ$'.format(lat),
+                                    va=va,
+                                    ha='left',
+                                    transform=ccrs.PlateCarree())
 
-
-    def set_gridlines(self, dx=60, dy=30, xlims=(-180, 180), ylims=(-90, 90)):
+    def set_gridlines(self,
+                      label_fmt="default",
+                      dx=60,
+                      dy=30,
+                      xlims=[-180, 180],
+                      ylims=[-90, 90],
+                      ynudge=5,
+                      reverse=False,
+                      **kwargs):
         '''
         Sets the gridlines going through the skymap and draw the labels.
 
+        :param label_fmt: the format type to draw labels with. "mpl" is the default option,
+                    along with 'TA', which defines lon \in [360, 0].
         :param: dx, dy : spacing between points (default 60, 30)
         :param xlims, ylims: tuple of limits of longitude and latitude (default +-180, +-90)
 
         Note: if lon_0 < 0, ylocs shift to the right instead (bug that needs to be fixed).
         '''
-        xlocs = np.arange(xlims[0], xlims[1], dx)
-        ylocs = np.arange(ylims[0] - dy, ylims[1] + dy, dy)
+        # needs to be fixed for gridlines reasons
+        xlocs = np.arange(-180, 180, dx)
+
+        # use preset xlims / ylims
+        if label_fmt == "default":
+            xlims = np.array([0, 360])
+            ylims = np.array([-90, 90])
+            reverse = False
+        if label_fmt == "mpl":
+            xlims = np.array([-180, 180])
+            ylims = np.array([-90, 90])
+            reverse = False
+        elif label_fmt == "TA":
+            xlims = np.array([-180, 180])
+            ylims = np.array([-90, 90])
+            reverse = True
+        elif label_fmt == "custom":
+            xlims = np.array(xlims)
+            ylims = np.array(ylims)
+            reverse = reverse
+
+        ylocs = np.arange(ylims[0], ylims[1], dy)
+
         ytick_fmt = ticker.StrMethodFormatter(r"${x}^\circ$")
-        gl = self.ax.gridlines(crs=ccrs.PlateCarree(), xlocs=xlocs, ylocs=ylocs, yformatter=ytick_fmt)
-        gl.ylabels_left = True
-        self.add_grid_labels(dx, dy, xlims, ylims, lft=False, mid=True)
+        gl = self.ax.gridlines(draw_labels=False,
+                               crs=ccrs.PlateCarree(),
+                               xlocs=xlocs,
+                               ylocs=ylocs,
+                               yformatter=ytick_fmt,
+                               xformatter=ytick_fmt,
+                               y_inline=False,
+                               **kwargs)
+        self.add_grid_labels(dx,
+                             dy,
+                             xlims,
+                             ylims,
+                             lft=True,
+                             rgt=False,
+                             mid=True,
+                             ynudge=ynudge,
+                             reverse=reverse)
         # self.ax.gridlines(xlocs=xlocs, ylocs=ylocs)
         # self.add_grid_labels(self.lon_0 + xlocs, ylocs, lft=True, mid=True)
 
@@ -164,6 +255,10 @@ class AllSkyMapCartopy():
         '''
         return self.ax.legend(**kwargs)
 
+    def title(self, title, **kwargs):
+        '''wrapper for ax.set_title()'''
+        self.ax.set_title(title, **kwargs)
+
     def geodesic(self, lon1, lat1, lon2, lat2, **kwargs):
         '''
         Plot geodesics onto the skymap. lons / lats are assumed to be from
@@ -176,15 +271,17 @@ class AllSkyMapCartopy():
         '''
         # shift lons by 180 due to cartopy / mpl plotting differences
         lon1, lon2 = 180. - lon1, 180. - lon2
-        self.ax.plot([lon1, lon2], [lat1, lat2], transform=ccrs.Geodetic(), **kwargs)
+        self.ax.plot([lon1, lon2], [lat1, lat2],
+                     transform=ccrs.Geodetic(),
+                     **kwargs)
 
     def tissot(self, lon, lat, rad, npts=100, **kwargs):
         '''
         Plot tissot onto skymap. lon / lat are assumed to be from SkyCoord objects.
 
-        :param lon: longitudes of each point ([0, 360])
+        :param lon: longitudes of each point ([-180, 180])
         :param lat: latitudes of each point ([-90, 90])
-        :param rad: size of each point (basemap 10.0 \approx 20)
+        :param rad: size of each point (basemap 10.0 \approx 20 \approx 1 degree)
         :param npts: number of points at each point
         '''
         # shift longitudes by 180.
@@ -193,7 +290,7 @@ class AllSkyMapCartopy():
         # multiply radius by 55 (base unit)
         rad *= 55. * 2.  # *2 to account for double radius size
 
-        self.ax.tissot(rad, lon, lat, npts, **kwargs)
+        return self.ax.tissot(rad, lon, lat, npts, **kwargs)
 
     def scatter(self, lons, lats, color, **kwargs):
         '''
@@ -201,64 +298,111 @@ class AllSkyMapCartopy():
         longitudes and latitudes from SkyCoord, so that lons / lats are defined
         in that sense.
 
-        :param lons: longitudes of each point ([0, 360])
+        :param lons: longitudes of each point ([-180, 180])
         :param lats: latitudes of each point ([-90, 90])
         :param color: either color of all points or some array for color mapping
         '''
-        self.ax.scatter(180. - lons, lats, c=color, transform=ccrs.PlateCarree(), **kwargs)
+        return self.ax.scatter(180. - lons,
+                               lats,
+                               c=color,
+                               transform=ccrs.PlateCarree(),
+                               **kwargs)
 
-    def exposure_limit(self, limiting_dec, num_points=10000, **kwargs):
+    def contourf(self, lons, lats, vals, **kwargs):
+        '''Plot filled contour in skymap'''
+        return self.ax.contourf(lons,
+                                lats,
+                                vals,
+                                transform=ccrs.PlateCarree(),
+                                **kwargs)
+
+    def exposure_limit(self,
+                       limiting_dec,
+                       coord="G",
+                       num_points=10000,
+                       **kwargs):
         '''
         Plot limit of exposure of given observatory.
 
         :param limiting_dec: declination of limit of exposure in degrees
+        :param coord: the coordinate system to plot it in ("G" = galactic, "E"= equatorial).
+            - galactic coords in lon, lat \in [-180, 180], [-90, 90]
+            - equatorial coords in ra, dec \in [360, 0], [-90, 90] (like in TA paper)
         :param num_points: number of points to plot with
         '''
         rightascensions = np.linspace(-180, 180, num_points)
         boundary_decs = np.tile(limiting_dec, num_points)
-        c = SkyCoord(ra = rightascensions * u.degree,
-                    dec = boundary_decs * u.degree, frame = 'icrs')
-        exp_lon = c.galactic.l.deg
-        exp_lat = c.galactic.b.deg
+        c = SkyCoord(ra=rightascensions * u.degree,
+                     dec=boundary_decs * u.degree,
+                     frame='icrs')
 
         # shift by 180. since SkyCoord defines from [0, 360]
-        self.ax.scatter(180. - exp_lon, exp_lat, transform=ccrs.PlateCarree(), **kwargs)
+        if coord == "G":  # galactic
+            x = 180. - (180. - c.galactic.l.deg)
+            y = c.galactic.b.deg
+        elif coord == "E":  # equatorial
+            x = 180. - c.icrs.ra.deg
+            y = c.icrs.dec.deg
 
-    def exposure_map(self, detector_params, num_points=220, **kwargs):
+        return self.ax.scatter(x, y, transform=ccrs.PlateCarree(), **kwargs)
+
+    def exposure_map(self,
+                     detector_params,
+                     coord="G",
+                     num_points=220,
+                     **kwargs):
         '''
         Plot exposure of given observatory as contour map
 
         :param detector_params: parameters used to evaluate exposure function
+        :param coord: the coordinate system to plot it in ("G" = galactic, "E"= equatorial).
+            - galactic coords in lon, lat \in [-180, 180], [-90, 90]
+            - equatorial coords in ra, dec \in [360, 0], [-90, 90] (like in TA paper)
         :param num_points: number of points to plot with
         '''
         rightascensions = np.linspace(-180, 180, num_points)
-        declinations = np.linspace(-np.pi/2, np.pi/2, num_points)
+        declinations = np.linspace(-np.pi / 2, np.pi / 2, num_points)
 
         # exposure function in full declination width
         m_full = np.asarray([m_dec(d, detector_params) for d in declinations])
         exposure_factor = m_full / np.max(m_full)
 
-        exp_cmap = colors.LinearSegmentedColormap.from_list('custom', 
-                                                        [lightgrey, grey], N = 6)
-        norm_proj = colors.Normalize(exposure_factor.min(), exposure_factor.max())
+        exp_cmap = colors.LinearSegmentedColormap.from_list('custom',
+                                                            [lightgrey, grey],
+                                                            N=6)
+        norm_proj = colors.Normalize(exposure_factor.min(),
+                                     exposure_factor.max())
 
         for dec, proj in np.nditer([declinations, exposure_factor]):
             decs = np.tile(dec, num_points)
-            c = SkyCoord(ra = rightascensions * u.rad,
-                        dec = decs * u.rad, frame = 'icrs')
-            lon = 180. - c.galactic.l.deg
-            lat = c.galactic.b.deg
+            c = SkyCoord(ra=rightascensions * u.rad,
+                         dec=decs * u.rad,
+                         frame='icrs')
+
+            if coord == "G":
+                x = 180. - c.galactic.l.deg
+                y = c.galactic.b.deg
+            elif coord == "E":
+                x = 180. - c.icrs.ra.deg
+                y = c.icrs.dec.deg
+
             if (proj == 0):
-                self.ax.scatter(lon, lat, transform=ccrs.PlateCarree(), linewidth = 3,
-                            color = white, alpha = 1)
+                self.ax.scatter(180. - x,
+                                y,
+                                transform=ccrs.PlateCarree(),
+                                linewidth=3,
+                                color=white,
+                                alpha=1)
             else:
-                self.ax.scatter(lon, lat, transform=ccrs.PlateCarree(), linewidth = 3,
-                            color = exp_cmap(norm_proj(proj)), alpha = 1)
+                self.ax.scatter(180. - x,
+                                y,
+                                transform=ccrs.PlateCarree(),
+                                linewidth=3,
+                                color=exp_cmap(norm_proj(proj)),
+                                alpha=1)
 
-
-    def save(self, filename):
+    def save(self, filename, dpi=300, **kwargs):
         '''
         Saves the skymap to a given filename.
         '''
-        self.fig.savefig(filename, bbox_inches='tight')
-
+        self.fig.savefig(filename, bbox_inches="tight", dpi=dpi, **kwargs)
